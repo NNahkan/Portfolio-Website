887685ab759883a497bd1a3a829390cc2c0f9423
NNahkan <hoksuz2424@gmail.com>
2023-09-04 18:03:39 -0400

    Working on HomePage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import s from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={s.homePageContainer}>
+      <h1 className={s.homeHeader}>Hi there! 👋</h1>
+      <p className={s.homeParap}>
+        I'm Ali Hakan Oksuz, a seasoned frontend developer with over 2 years of
+        experience. I'm passionate about crafting exceptional web experiences
+        and turning lines of code into engaging digital journeys.
+      </p>
+      <p className={s.homeParap}>
+        {" "}
+        My love for clean and minimalistic designs guides my work, where I find
+        beauty in simplicity and order.If you'd like to chat or collaborate,
+        feel free to reach out. Let's bring your web projects to life! Feel free
+        to personalize it further to better reflect your unique personality and
+        experiences.
+      </p>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Link } from "react-router-dom";
+import s from "./Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-flex">
-        <Link className="navbar-item" to="/">
+    <nav className={s.navbar}>
+      <div className={s.navbarFlex}>
+        <Link className={s.navbarItem} to="/">
           <img src="https://avatars.githubusercontent.com/u/80037793?v=4" alt="" />
         </Link>
-        <ul className="navbar-list">
-          <li className="navbar-item">
+        <ul className={s.navbarList}>
+          <li className={s.navbarItem}>
             <Link to="/">Home</Link>
           </li>
-          <li className="navbar-item">
+          <li className={s.navbarItem}>
             <Link to="/contact">Contact</Link>
           </li>
-          <li className="navbar-item">
+          <li className={s.navbarItem}>
             <Link to="/about">About</Link>
           </li>
-          <li className="navbar-item">
+          <li className={s.navbarItem}>
             <Link to="/about">Fake project</Link>
           </li>
         </ul>
