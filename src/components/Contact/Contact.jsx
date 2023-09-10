09e9e3c50e2b9e0dbd8fcfb0ac51a57bf28c3678
NNahkan <hoksuz2424@gmail.com>
2023-09-10 15:04:41 -0400

    Styling contact component.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,33 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import s from "./Contact.module.css";
 
 const Contact = () => {
+  const formData = [
+    { type: "text", name: "name" },
+    { type: "email", name: "email" },
+  ];
   return (
-    <div>
+    <div className="container">
       <h1>Contact</h1>
+      <p>Get in touch or shoot me an email directly on hoksuz2424@gmail.com</p>
+      <form className={s.contactForm} action="">
+        {formData.map((item) => (
+          <input
+            className={s.inputForm}
+            key={item.name}
+            type="text"
+            placeholder={item.name}
+          />
+        ))}
+        <textarea
+          className={`${s.inputForm} ${s.messageArea}`}
+          name="message"
+          placeholder="Message"
+          rows="8"
+        ></textarea>
+        <button>Send Message</button>
+      </form>
     </div>
   );
 };
